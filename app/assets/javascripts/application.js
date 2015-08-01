f09be0d7638164300c00e29acf0ada8d846f00b1
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-01 21:26:07 +0900

    config of application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require uikit
+//= require select2
 //= require turbolinks
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -17,19 +17,21 @@ Bundler.require(*Rails.groups)
 
 module Darkness
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.preferred_syntax = :sass
+    config.sass.syntax = :sass
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ja
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.generators.template_engine = :slim
+    config.generators.helper          = false
+    config.generators.assets          = false
+
+    config.git_revision = `git log --abbrev-commit --pretty=oneline | head -1 | cut -d' ' -f1`
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.generators.test_framework = 'rspec'
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
