edaf3e93dd0f92d8dd2bbb0f765baad5e6d984ed
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-15 05:01:45 +0900

    [modify] page_numがnilのとき 0, img_pathはモデルでつくる

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,8 +6,9 @@ class Album < ActiveRecord::Base
     directory = Rails.root.join('public', 'images', id.to_s)
     system("mkdir #{directory}")
     system("unzip #{zip_path} -d #{directory}")
-    Dir.glob("#{directory}/**/*.{jpeg,png,jpg}").sort.each_with_index do |img_path, count|
-      Photo.create(album_id: id, img_path: img_path, page: count + 1)
+    Dir.glob("#{directory}/**/*.{jpeg,png,jpg}").sort.each_with_index do |raw_path, count|
+      path = raw_path.gsub(Rails.root.join('public').to_s, '')
+      Photo.create(album_id: id, img_path: path, page: count + 1)
     end
     update(page_num: Photo.last.page)
   end
