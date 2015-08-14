05f8b56e0162e0e65185d9ac2d1dc58e21cfdc1e
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-15 04:36:51 +0900

    [add] 画像を閲覧できるように

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,10 +3,10 @@ class Album < ActiveRecord::Base
   belongs_to :event
 
   def unzip_and_bind_photos
-    directory = Rails.root.join('public', id.to_s)
+    directory = Rails.root.join('public', 'images', id.to_s)
     system("mkdir #{directory}")
     system("unzip #{zip_path} -d #{directory}")
-    Dir.glob("#{directory}/*.{jpeg,png,jpg}").sort.each_with_index do |img_path, count|
+    Dir.glob("#{directory}/**/*.{jpeg,png,jpg}").sort.each_with_index do |img_path, count|
       Photo.create(album_id: id, img_path: img_path, page: count + 1)
     end
     update(page_num: Photo.last.page)
