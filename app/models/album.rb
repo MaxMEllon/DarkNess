b1948fed2240a45cf399a9e79dcde99355aca59d
MaxMEllon <mozi_kke@excite.co.jp>
2015-08-15 01:00:48 +0900

    [add] albumにphotoを紐付ける

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,7 +6,8 @@ class Album < ActiveRecord::Base
     directory = Rails.root.join('public', self.id.to_s)
     system("mkdir #{directory}")
     system("unzip #{self.zip_path} -d #{directory}")
-    Dir.glob("#{directory}/*.{jpeg,png,jpg}").each do |img_path|
+    Dir.glob("#{directory}/*.{jpeg,png,jpg}").sort.each_with_index do |img_path, count|
+      Photo.create(album_id: self.id, img_path: img_path, page: count + 1)
     end
   end
 end
